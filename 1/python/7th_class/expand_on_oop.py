b7788af8057b106a9dd01cc85d08ba5d420f9667
Afif An Nafie <drarinaislam101190@gmail.com>
2022-05-19 17:52:44 +0300

    Adding Latest Class Activities

--- 1/python/7th_class/expand_on_oop.py
+++ 1/python/7th_class/expand_on_oop.py
@@ -8,8 +8,19 @@ class Student():  # you can use brackets right after the name if you want to. Th
     dept = ""
 
     # __init__ method (i like to call functions, methods inside a class) will run the code in it at the time of creating the object. It is also a builin function.
+    # The method also takes arguments when creating objects (i will talk about objects later).
     def __init__(self):
-        pass
+        print(
+            f"This is a Student named {self.name}, and their department is {self.dept}")
 
     def getName(self, name=name):
         return name
+
+
+# These are objects, When using class you have to make class, this is how to make them:
+rami = Student()  # So, it is like creating variables, right? We assign a name to this object, then we write the name of the class and inside the bracket you can put the parameters that the __init__ method will take in.
+# Now we have a Student object and it has all the characteristics of the actual class
+# To use anything we have to use a dot after the object name and then the method or attribute (attributes are variable, no difference!!) name:
+rami.dept = "Student"
+rami.name = "Rami Harami"
+rami.__init__()  # You have to use brackets after a method name, or else it won't work!
