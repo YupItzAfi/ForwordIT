abd8a76be55ce8c847cacc28e5eb57004cca131e
Afif An Nafie <drarinaislam101190@gmail.com>
2022-05-19 19:14:42 +0300

    Added Changes

--- 1/python/9th_class/linked_list.py
+++ 1/python/9th_class/linked_list.py
@@ -14,6 +14,19 @@ class Linked_List:
             print(temp.data)
             temp = temp.next
 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insertAfterNode(self, prev_data, data):
+        if prev_data is None:
+            print("no previous data is there.")
+            return None
+        new_node = Node(data)
+        new_node.next = prev_data.next
+        prev_data.next = new_node
+
 
 lst = Linked_List()
 lst.head = Node(10)
@@ -22,5 +35,8 @@ third = Node(30)
 
 lst.head.next = sec
 sec.next = third
+lst.insertAfterNode(sec, 200)
+
+lst.push(87967)
 
 lst.print()
