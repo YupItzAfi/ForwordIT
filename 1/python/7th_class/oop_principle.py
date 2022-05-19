bc86b3c889181a8f5f4b03f8552dfc7b008b0ab9
Afif An Nafie <drarinaislam101190@gmail.com>
2022-05-19 18:25:28 +0300

    Changes

--- 1/python/7th_class/oop_principle.py
+++ 1/python/7th_class/oop_principle.py
@@ -1,1 +1,21 @@
 # oop principles = Encapsulation, Inheritance, Polymorphism, Abstact(ion)
+
+from abstraction_example import Student
+
+
+class StudentImpl(Student):
+    name = ""
+    dept = ""
+
+    def __init__(self):
+        print(
+            f"This is a Student named {self.name}, and their department is {self.dept}")
+
+    def getName(self, name=name):
+        return name
+
+
+rami = StudentImpl()
+rami.dept = "Student"
+rami.name = "Rami Harami"
+rami.__init__()
