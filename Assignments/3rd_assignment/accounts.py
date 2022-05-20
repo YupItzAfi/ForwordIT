f0f9bd886fb33ce8daca7434d08fd8fa0837c4ec
Afif An Nafie <drarinaislam101190@gmail.com>
2022-05-20 18:25:58 +0300

    Update on Banking Assignment

--- Assignments/3rd_assignment/accounts.py
+++ Assignments/3rd_assignment/accounts.py
@@ -1,5 +1,20 @@
+from random import random
+
+
+from random import random
+
+
 class SavingsAccount:
-    account_number = 0
+    account_number = random()
     opening_balance = 0
     deposit = 0
     withdraw = 0
+
+    def __init__(self) -> None:
+        pass
+
+    def deposit_money(self, amount):
+        self.opening_balance = amount
+
+    if deposit != 0:
+        deposit_money(deposit)
--- Assignments/3rd_assignment/customer.py
+++ Assignments/3rd_assignment/customer.py
@@ -1,16 +1,12 @@
-from random import random
-
-
 class Customer:
     name = ""
     age = 18
-    nid = random()
+    nid = 0
     address = ""
 
     # Other Variables
 
     temp1, temp2, temp3, temp4 = "", 18, 0, ""
-    system = None
 
     def __init__(self, name, age, nid, address):
         if age < 18:
@@ -23,59 +19,81 @@ class Customer:
             self.address = address
             self.temp1, self.temp2, self.temp3, self.temp4 = self.name, self.age, self.nid, self.address
 
-            if self.nid == nid:
-                self.system = Customer("", 18, 0, "")
             print(f"Hi {self.name}, This is your CLI Bank Account Management System, Type your command below. PS: Type 'help' for commands")
-            self.system.__command_enterer()
-
-    def updateInfo(self):
-        print("Update Your Info, Enter your New Details below:")
-        for i in range(4):
-            if i == 1:
-                self.name = str(input("Enter your New Name: "))
-            if i == 2:
-                self.age = int(input("Enter your New Age: "))
-            if i == 3:
-                self.nid = int(input("Enter your New NID: "))
-            if i == 4:
-                self.address = str(input("Enter your New Address: "))
-        print("Your Account Information Updated Succesfully! To Revert Action Type 'revert info'")
-
-    def openSavingsAccount(self):
-        from accounts import SavingsAccount
-        savingsAcc = SavingsAccount()
-
-    def revert_account(self):
-        if input("Are you sure you want to revert your accounts info to the previous one? If yes, type 'yes'. anything else would be a no: ") == "yes":
-            self.name, self.age, self.nid, self.address = self.temp1, self.temp2, self.temp3, self.temp4
-        else:
-            return
+            self.__command_enterer()
 
     def __CommandsManager(self, command=""):
+        if command.lower() == 'help':
+            print("""
+=== Bank Account Management System Help ===
+help                --  This Help Window.
+update info            --  Updates Your Info to a new one.
+create savings account --  Create a Savings Account.
+revert info            --  Revert your Changed Info.
+show info              --  Shows your Bank Account Details such as current balance, NID and name.
+delete account         --  Deletes your Account.
+exit                   --  Exits The Application.
+""")
+
+        if command.lower() == 'update info':
+            print("Update Your Info, Enter your New Details below:")
+            for i in range(4):
+                if i == 1:
+                    self.name = str(input("Enter your New Name: "))
+                if i == 2:
+                    self.age = int(input("Enter your New Age: "))
+                if i == 3:
+                    self.nid = int(input("Enter your New NID: "))
+                if i == 4:
+                    self.address = str(input("Enter your New Address: "))
+            print(
+                "Your Account Information Updated Succesfully! To Revert Action Type 'revert info'")
+            return
+
+        if command.lower() == 'create savings account':
+            from accounts import SavingsAccount
+            global __savingsAcc
+            __savingsAcc = SavingsAccount()
+            return
+
+        if command.lower() == 'revert info':
+            ubci = input(
+                "Are you sure you want to revert your accounts info to the previous one? If yes, type 'yes'. anything else would be a no: ")
+            if ubci.lower == 'yes':
+                self.name, self.age, self.nid, self.address = self.temp1, self.temp2, self.temp3, self.temp4
+            else:
+                return
+
+        if command.lower() == 'show info':
+            print(f"Your Name is {self.name}")
+            print(f"Your NID is {self.nid}")
+            print(f"Balance is {__savingsAcc.opening_balance}")
+            return
+
+        if command.lower() == 'deposit money':
+            self.deposit(input("How much money you want to deposit? "))
+            return
+
+        if command.lower() == 'exit':
+            print("Goodbye!")
+            exit()
+
         if command == "":
             print("There is no Command")
-        elif command.lower == 'help' or '?':
-            print("""
-            === Bank Account Management System Help ===
-            ?, help                --  This Help Window.
-            update info            --  Updates Your Info to a new one.
-            create savings account --  Create a Savings Account.
-            revert info            --  Revert your Changed Info
-            show info              --  Shows your Bank Account Details such as current balance, NID and name
-            delete account         --  Deletes your Account
-            """)
-        elif command.lower == 'update info':
-            system.updateInfo()
-        elif command.lower == 'create savings account':
-            system.openSavingsAccount()
-        elif command.lower == 'revert info':
-            system.revert_account()
+            return
 
-    def __command_enterer():
-        for i in range(999):
-            global input
-            input = str(input("=> "))
-            system.__CommandsManager(input)
+        else:
+            print(f"The command \"{command}\" does not exist")
+            return
+
+    def deposit(self, amount):
+        print(f"Depositing {amount} Taka To Savings Account")
+        __savingsAcc.deposit = amount
+
+    def __command_enterer(self):
+        for i in range(999999):
+            command = input("=> ")
+            self.__CommandsManager(command)
 
 
 class CustomerImpl(Customer):
