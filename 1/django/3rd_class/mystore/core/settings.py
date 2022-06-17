0a13bca959b25fe660d8b53d23607e46547ae692
Afif An Nafie <drarinaislam101190@gmail.com>
2022-06-17 05:23:49 +0300

    Fixing Django Security Key issue all

--- 1/django/3rd_class/mystore/core/settings.py
+++ 1/django/3rd_class/mystore/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from decouple import config
 import os
 from pathlib import Path
 
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x%z#rk%aaz@n^)yb#iag6ec)(i^5)q!n+mc%l$=_^#0ww+5h8c'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
